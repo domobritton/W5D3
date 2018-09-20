e95ad361b1b4c5b4faf755a717739db2e1f53d65
domonic <domonicbritton@gmail.com>
2018-09-19 17:08:11 -0700

    Add session class methods

--- skeleton/lib/session.rb
+++ skeleton/lib/session.rb
@@ -4,16 +4,22 @@ class Session
   # find the cookie for this app
   # deserialize the cookie into a hash
   def initialize(req)
+    @req = req
   end
 
   def [](key)
+    key
+    # key is xyz or pho
   end
 
   def []=(key, val)
+    @req[key] = val
   end
 
   # serialize the hash into json and save in a cookie
   # add to the responses cookies
   def store_session(res)
+  
+    res.to_json
   end
 end
